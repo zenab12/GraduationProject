24daf9261642bfbe1782ea422b6a437551482b0c
zenab mohammed abdelmonaem <78083890+zenab12@users.noreply.github.com>
2021-04-20 00:27:43 +0200

    finshing sign in /up page except the font '

---
+++ js/pppp.js
@@ -0,0 +1,50 @@
+$(document).ready(function() {
+
+    $('a.Menu').on('click', function() {
+
+        $('a.Menu').find('i').toggleClass('fa-times');
+        $(this).parent().prev().toggleClass('hidden')
+    });
+
+    $('li.services a').on('click', function() {
+
+
+        $(this).parent().find($('.servicesBox')).toggleClass('hidden');
+    });
+
+  $('li.services .fa-caret-down').on('click', function() {
+
+
+        $(this).parent().find($('.servicesBox')).toggleClass('hidden');
+    });
+    
+
+    if ($('body').width() <= 768) {
+
+        $('.navLinks > li a').on('click', function() {
+
+            $(this).parent().addClass('active').siblings().removeClass('active');
+            $(this).parent().addClass('active').siblings().find($('.servicesBox li')).removeClass('active').parent().addClass('hidden')
+
+
+
+        })
+
+
+              $('.navLinks > li .fa-caret-down').on('click', function() {
+
+            $(this).parent().addClass('active').siblings().removeClass('active');
+            $(this).parent().addClass('active').siblings().find($('.servicesBox li')).removeClass('active').parent().addClass('hidden')
+
+
+
+        });
+    }
+
+    $('.Sign .signLinks li').on('click', function() {
+
+         $($(this).data('class')).addClass('active').siblings().removeClass('active');
+
+        $("." + $(this).data('class')).removeClass('hiden').siblings().addClass('hiden');
+    })
+});
